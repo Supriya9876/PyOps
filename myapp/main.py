d72ea4b087e0aabfcdd37df16b3cc918663d9013
Supriya9876 <supriyabatabyal346@gmail.com>
2026-08-30 19:46:00 +0530

    Debugging 503 error

--- myapp/main.py
+++ myapp/main.py
@@ -16,7 +16,6 @@ DB_PASS = os.getenv("DB_PASS")                # no default — must come from .e
 
 app = FastAPI(title="Task Tracker Api")
 
-#Adding new feature
 class Task(BaseModel):
     name: str
     description: str = ""
